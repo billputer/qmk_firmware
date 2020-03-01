c85b0dac7b44bd02c2b5a8010c86df554d7ca092
Bill Wiens <bill.wiens@liquidplanner.com>
2020-03-01 13:37:25 -0800

    Switch from matrix_scan_user to layer_state_set_user like default Ergodox-EZ keymap

--- layouts/community/ergodox/billputer/keymap.c
+++ layouts/community/ergodox/billputer/keymap.c
@@ -1,6 +1,4 @@
 #include QMK_KEYBOARD_H
-#include "debug.h"
-#include "action_layer.h"
 #include "version.h"
 
 #define BASE 0 // default layer
@@ -92,50 +90,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-  [1] = ACTION_LAYER_TAP_TOGGLE(SYMB) // FN1 - Momentary Layer 1 (Symbols)
-};
+uint32_t layer_state_set_user(uint32_t state) {
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        register_code(KC_RSFT);
-      } else {
-        unregister_code(KC_RSFT);
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
+    uint8_t layer = biton32(state);
 
-// Runs just one time when the keyboard initializes.
-void matrix_init_user(void) {
-
-};
-
-// Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
-
-  uint8_t layer = biton32(layer_state);
-
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  switch (layer) {
-    case BASE:
-      break;
-
-    case SYMB:
-      ergodox_right_led_2_on();
-      ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
-      break;
-
-    default:
-      break;
-  }
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+      case 0:
+        // no lights on default layer
+        break;
+      case 1:
+        ergodox_right_led_2_on();
+        ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
+        break;
+      case 2:
+        ergodox_right_led_3_on();
+        ergodox_right_led_3_set(LED_BRIGHTNESS_LO);
+        break;
+      default:
+        break;
+    }
+    return state;
 
 };
+
+void keyboard_post_init_user(void) {
+  layer_state_set_user(layer_state);
+}
