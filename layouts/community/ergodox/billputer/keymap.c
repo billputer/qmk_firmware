112dcfc504db4f6e6ff9500ba42ddf4a8095720b
Bill Wiens <bill.wiens@liquidplanner.com>
2020-07-10 14:09:21 -0700

    Update layer toggling so that left button is one-shot, right button toggles through layers

--- layouts/community/ergodox/billputer/keymap.c
+++ layouts/community/ergodox/billputer/keymap.c
@@ -8,9 +8,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  | ESC  |           | P/P  |   6  |   7  |   8  |   9  |   0  | Bspace |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L2  |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  LCtrl |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |OSL(  |           |TG(L1)|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|  L1) |           |      |------+------+------+------+------+--------|
+ * |  LCtrl |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ;|L1 |   '    |
  * |--------+------+------+------+------+------| Enter|           | Del  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   Up |   /    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_ergodox(
   // left hand
   KC_GRAVE,       KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESCAPE,
-  KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
+  KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   OSL(1),
   KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
   KC_LSHIFT,      KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_ENTER,
   KC_LGUI,        KC_MUTE,     KC_VOLD,KC_VOLU, KC_SPACE,
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_LALT,KC_BSPACE,KC_PGDOWN,
   // right hand
   KC_MPLY,     KC_6,   KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPACE,
-  TG(2),       KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,     KC_BSLS,
+  TG(1),       KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,     KC_BSLS,
                KC_H,   KC_J,    KC_K,     KC_L,    LT(1,KC_SCLN),  KC_QUOT,
   KC_DELETE,   KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_UP,    KC_SLASH,
                        KC_RALT, KC_RCTRL, KC_LEFT, KC_DOWN,  KC_RIGHT,
@@ -44,12 +44,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQUAL,
   KC_MINUS, KC_ENTER, KC_SPACE
 ),
-/* Layer 1
+/* Layer 1: Symbols
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |  F11 |  F12 |  F13 |  F14 |  F15 |      |           |      |      |      |      |      |      |        |
+ * |        |  F11 |  F12 |  F13 |  F14 |  F15 |      |           | TG(2)|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |PrevT |      |NextT |      |      |------|           |------| Left | Down |  Up  | Right|      |        |
  * |--------+------+------+------+------+------|      |           |INSERT|------+------+------+------+------+--------|
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_TRNS,KC_TRNS,BL_DEC,
   // right hand
   KC_TRNS,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  TG(2),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS,
   KC_INS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -85,12 +85,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,
   KC_TRNS,  KC_KP_ENTER, KC_TRNS
 ),
-/* Layer 2
+/* Layer 2: Mouse
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |     |       |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |WHL_UP|      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |TO(0) |WHL_UP|      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------|M_LEFT|M_DOWN| M_UP |M_RGHT|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRNS, KC_TRNS, KC_TRNS,
   // right hand
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_MS_WH_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  TO(0),   KC_MS_WH_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
            KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT,KC_TRNS,KC_TRNS,
   KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                     KC_MS_BTN1, KC_MS_BTN2, KC_TRNS, KC_TRNS, KC_TRNS,
